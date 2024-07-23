4adfe9ddd90fb1f7427f6853fc8f1cb785d9efbd
subhajit-007 <subhajit.mandal@nagarro.com>
2024-07-23 11:05:55 +0530

    Add restaurant list

--- foodorder-fe/src/app/screens/customer/restaurant-list/restaurant-list.component.ts
+++ foodorder-fe/src/app/screens/customer/restaurant-list/restaurant-list.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatLabel, MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { Router, RouterModule } from '@angular/router';
+import { CustomerService } from '../../../services/customer/customer.service';
+
+
+const MatModules = [
+  MatToolbarModule,
+  MatLabel,
+  // MatFormFieldModule,
+  // MatInputModule,
+  MatButtonModule,
+  // MatIconModule,
+  MatListModule,
+  MatDividerModule,
+];
 
 @Component({
   selector: 'app-restaurant-list',
   standalone: true,
-  imports: [],
+  imports: [...MatModules, RouterModule, CommonModule],
   templateUrl: './restaurant-list.component.html',
   styleUrl: './restaurant-list.component.scss'
 })
-export class RestaurantListComponent {
+export class RestaurantListComponent implements OnInit {
+  restaurantList: any[] = [];
 
+  constructor(private customerService: CustomerService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.customerService.getRestaurants().subscribe({
+      next: (res) => {
+        console.log(res.data);
+        this.restaurantList = res.data;
+      },
+      error: (err) => {
+        console.error(err)
+      }
+    })
+  }
+
+  goToRestaurantDetailPage(restaurantId: Number): void {
+
+  }
 }
--- foodorder-fe/src/app/services/customer/customer.service.ts
+++ foodorder-fe/src/app/services/customer/customer.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AxiosService } from '../network/axios.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private axiosService: AxiosService) { }
+
+  getRestaurants(): Observable<any> {
+    return new Observable(observer => {
+      this.axiosService.get('/restaurant/list/').then(response => {
+        observer.next(response.data);
+        observer.complete();
+      }).catch(error => {
+        observer.error(error);
+      });
+    });
+  }
 }
