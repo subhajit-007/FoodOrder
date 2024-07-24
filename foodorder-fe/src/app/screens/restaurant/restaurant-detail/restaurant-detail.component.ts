394e1907dbde03c98a74438cbc7bd10dde6a3a73
subhajit-007 <subhajit.mandal@nagarro.com>
2024-07-24 12:08:42 +0530

    Update styles

--- foodorder-fe/src/app/screens/restaurant/restaurant-detail/restaurant-detail.component.ts
+++ foodorder-fe/src/app/screens/restaurant/restaurant-detail/restaurant-detail.component.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { RestaurantService } from '../../../services/restaurant/restaurant.service';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatLabel } from '@angular/material/form-field';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
+
+const MatModules = [
+  MatToolbarModule,
+  MatLabel,
+  // MatFormFieldModule,
+  // MatInputModule,
+  MatButtonModule,
+  // MatIconModule,
+  MatListModule,
+  MatDividerModule,
+  MatCardModule,
+];
 
 @Component({
   selector: 'app-restaurant-detail',
   standalone: true,
-  imports: [],
+  imports: [...MatModules, RouterModule, CommonModule],
   templateUrl: './restaurant-detail.component.html',
   styleUrl: './restaurant-detail.component.scss'
 })
-export class RestaurantDetailComponent {
+export class RestaurantDetailComponent implements OnInit {
+  restaurant: any;
+  menuList: any[] = [];
+  restaurantId: string = ""
+
+  constructor(private route: ActivatedRoute, private restaurantService: RestaurantService) { }
+
+  ngOnInit(): void {
+    this.restaurantId = this.route.snapshot.paramMap.get('restaurantId') ?? '';
+    console.log(Number(this.restaurantId))
+    this.getRestaurantInfo()
+    this.getRestaurantMenu()
+  }
+
+  getRestaurantInfo(): void {
+    this.restaurantService.getRestaurantDetails(this.restaurantId).subscribe({
+      next: (res) => {
+        this.restaurant = res.data
+        console.log(this.restaurant)
+      },
+      error: (err) => {
+        console.error(err)
+      }
+    })
+  }
+
+  getRestaurantMenu(): void {
+    this.restaurantService.getMenuListByRestaurantId(this.restaurantId).subscribe({
+      next: (res) => {
+        this.menuList = res.data
+        console.log(this.menuList)
+      },
+      error: (err) => {
+        console.error(err)
+      }
+    })
+  }
 
 }
--- foodorder-fe/src/app/services/restaurant/restaurant.service.ts
+++ foodorder-fe/src/app/services/restaurant/restaurant.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { AxiosService } from '../network/axios.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantService {
 
-  constructor() { }
+  constructor(private axiosService: AxiosService) { }
+
+  getRestaurantDetails(restaurantId: any): Observable<any> {
+    return new Observable(observer => {
+      this.axiosService.get(`/restaurant/${restaurantId}/details`).then(response => {
+        observer.next(response.data);
+        observer.complete();
+      }).catch(error => {
+        observer.error(error);
+      });
+    });
+  }
+
+  getMenuListByRestaurantId(restaurantId: any): Observable<any> {
+    return new Observable(observer => {
+      this.axiosService.get(`/restaurant/${restaurantId}/menus`).then(response => {
+        observer.next(response.data);
+        observer.complete();
+      }).catch(error => {
+        observer.error(error);
+      });
+    });
+  }
+
 }
